2bb925cb2be6de7a4543c6d403d882eb27d4ff71
Mike Admire <mikeadmire@gmail.com>
2011-07-08 22:34:38 -0500

    added date string to files that are moved for backups

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
+date=`date "+%Y%m%d-%H%M%S"`
 cd ~
 if [ -d ~/dotfiles ]
 then
-  mv ~/dotfiles ~/dotfiles.old
+  mv ~/dotfiles ~/dotfiles.$date
 fi
 git clone git://github.com/mikeadmire/dotfiles.git ~/dotfiles
 for i in .bash_aliases .bash_profile .gemrc .gvimrc.local .janus.rake .bashrc .vimrc.local
 do
   if [ -e $i ] || [ -h $i ]
   then
-    mv $i $i.old
+    mv $i $i.$date
   fi
   ln -s ~/dotfiles/$i
 done
