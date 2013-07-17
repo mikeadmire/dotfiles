b5e535402e9f4fc24f370897cb9b3720c454c32d
Mike Admire <mike@mikeadmire.com>
2013-07-16 21:13:14 -0500

    Add bundler-exec to install script.

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ then
   mv ~/dotfiles ~/dotfiles.$date
 fi
 git clone git://github.com/mikeadmire/dotfiles.git ~/dotfiles
+
+# add bundler-exec to .bashrc file
+curl -L https://github.com/gma/bundler-exec/raw/master/bundler-exec.sh > ~/dotfiles/.bundler-exec.sh
+echo "[ -f ~/dotfiles/.bundler-exec.sh ] && source ~/dotfiles/.bundler-exec.sh" >> ~/dotfiles/.bashrc
+
+# sym link dotfiles to home directory
 for i in .bash_aliases .bash_profile .gemrc .bashrc .autotest
 do
   if [ -e $i ] || [ -h $i ]
