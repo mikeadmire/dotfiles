e3331bd1509b12409f99dd12c374f9e0a042a69e
Mike Admire <mikeadmire@gmail.com>
2020-01-19 10:53:42 -0700

    Update install file.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ fi
 git clone git://github.com/mikeadmire/dotfiles.git ~/dotfiles
 
 # sym link dotfiles to home directory
-for i in .bash_aliases .bash_profile .gemrc .bashrc .autotest .tmux.conf .irbrc .zshrc .gitignore_global .gitconfig
+for i in .bash_aliases .bash_profile .bashrc .zshrc .gitignore_global .gitconfig
 do
   if [ -e $i ] || [ -h $i ]
   then
@@ -19,8 +19,9 @@ ZSH_PLUGIN_DIR='.oh-my-zsh/custom/plugins'
 if [ -d $ZSH_PLUGIN_DIR ]; then
   if [ -L $ZSH_PLUGIN_DIR ]; then
     rm $ZSH_PLUGIN_DIR
+    ln -s ~/dotfiles/zsh-plugins $ZSH_PLUGIN_DIR
   else
     rm -rf $ZSH_PLUGIN_DIR
+    ln -s ~/dotfiles/zsh-plugins $ZSH_PLUGIN_DIR
   fi
 fi
-ln -s ~/dotfiles/zsh-plugins $ZSH_PLUGIN_DIR
